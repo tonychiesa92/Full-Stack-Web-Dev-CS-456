85dd77a04f8c67646ec4e380283fcb309718b236
Tony C <tonychiesa92@gmail.com>
2022-08-14 02:45:06 -0400

    module 6

--- travlr/app.js
+++ travlr/app.js
@@ -37,10 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Allow CORS
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
