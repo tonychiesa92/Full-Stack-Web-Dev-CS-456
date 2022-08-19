2371dad214a0d5549995a069efebfb63e27240f4
Tony C <tonychiesa92@gmail.com>
2022-08-19 12:27:59 -0400

    finish mod 6

--- travlr/app.js
+++ travlr/app.js
@@ -1,5 +1,3 @@
-
-
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -7,15 +5,11 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const hbs = require("hbs");
 
-
 require("./app_api/database/db");
-
-
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
-
 
 const apiRouter = require("./app_api/routes/index");
 
@@ -33,14 +27,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
 // Allow CORS
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
